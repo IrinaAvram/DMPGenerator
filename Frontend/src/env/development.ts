bf0c9f67f8dce9306e52df0d0d5327a2a97b1393
Gabriel Dulca <gabrieldulca@gmail.com>
2018-04-26 15:14:45 +0200

    changed port for the Backend API endpoint

--- Frontend/src/env/development.ts
+++ Frontend/src/env/development.ts
@@ -1,5 +1,5 @@
 export const devVariables = {
-  apiEndpoint: 'http://localhost:4001',
+  apiEndpoint: 'http://localhost:8080',
   environmentName: 'Development Environment',
   ionicEnvName: 'dev'
 };
